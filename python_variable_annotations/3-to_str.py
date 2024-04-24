c88570327bac025334a971a57091bf553f315352
sachihiroo <saif3antar@gmail.com>
2024-04-24 23:35:36 +0100

    Basic annotations - to string

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+a type-annotated function floor.
+returns the floor of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """converts a number to string."""
+    return str(n)
