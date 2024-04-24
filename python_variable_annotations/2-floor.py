191a245c03ccc63487a699bfe80224545935a388
sachihiroo <saif3antar@gmail.com>
2024-04-24 23:28:37 +0100

    Basic annotations - floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+a type-annotated function floor.
+returns the floor of the float.
+"""
+
+
+def floor(n: float) -> int:
+    """concatenate two strings."""
+    return int(n)
