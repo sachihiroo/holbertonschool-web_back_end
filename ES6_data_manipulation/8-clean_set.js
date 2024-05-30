cf32c00c86ef285db41a338cb27f787fa4d82145
sachihiroo <saif3antar@gmail.com>
2024-05-30 17:12:39 +0100

    Clean set

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,12 @@
 export default function cleanSet(set, startString) {
-    if (startString === '' || typeof startString !== 'string') {
-      return '';
-    }
-    const result = [];
-    set.forEach((value) => {
-      if (value.startsWith(startString)) {
-        result.push(value.slice(startString.length));
-      }
-    });
-    return result.join('-');
+  if (startString === '' || typeof startString !== 'string') {
+    return '';
   }
+  const result = [];
+  set.forEach((value) => {
+    if (value.startsWith(startString)) {
+      result.push(value.slice(startString.length));
+    }
+  });
+  return result.join('-');
+}
