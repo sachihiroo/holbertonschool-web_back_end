0df52b78600f83dc8d0aa35d0fb126259f4b24a1
sachihiroo <saif3antar@gmail.com>
2024-05-30 01:42:28 +0100

    Handle multiple successful promises

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -2,7 +2,7 @@ import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
   // Use Promise.all to wait for both promises to resolve
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then((result) => {
       console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`);
     })
