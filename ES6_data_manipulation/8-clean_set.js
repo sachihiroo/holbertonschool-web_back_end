7f741ab2bc58a91f6469da252922e8a3259c2034
sachihiroo <saif3antar@gmail.com>
2024-05-30 17:10:56 +0100

    Clean set

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,12 @@
 export default function cleanSet(set, startString) {
-  // Check if startString is empty
-  if (startString === '') return '';
-
-  // Filter and map the set to get the substrings after startString
-  const substrings = Array.from(set)
-    .filter((value) => typeof value === 'string' && value.startsWith(startString))
-    .map((value) => value.substring(startString.length).trim());
-
-  // Join the substrings with a dash
-  return substrings.join('-');
-}
+    if (startString === '' || typeof startString !== 'string') {
+      return '';
+    }
+    const result = [];
+    set.forEach((value) => {
+      if (value.startsWith(startString)) {
+        result.push(value.slice(startString.length));
+      }
+    });
+    return result.join('-');
+  }
