e097d9bf005e315ca0e27bd59c3ded104dbbeeb7
sachihiroo <saif3antar@gmail.com>
2024-05-30 01:14:56 +0100

    Catch me if you can!

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,14 +1,14 @@
 export default function handleResponseFromAPI(promise) {
-  // Handler for when the promise resolves
-  promise.then(() => {
-    console.log('Got a response from the API');
-    // Return the desired object on resolution
-    return { status: 200, body: 'success' };
-  })
+  return promise
+    .then(() => {
+      console.log('Got a response from the API');
+      // Return the desired object on resolution
+      return { status: 200, body: 'success' };
+    })
   // Handler for when the promise rejects
     .catch(() => {
       console.log('Got a response from the API');
       // Return an empty Error object on rejection
-      return {};
+      return (new Error());
     });
 }
